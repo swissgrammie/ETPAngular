12663b8546f5c5244f1dbaf60ccdff1b6a80ee86
Graham Cunningham <graham.cunningham@itwks.com>
2016-07-06 22:09:39 +0200

    test service changes

--- src/app/amort.service.ts
+++ src/app/amort.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -7,8 +7,27 @@ export class AmortService {
 
   constructor(private http: Http) {}
 
+  
+
+
   getAmortSchedule() {
-    return this.http.get('/app/amortSchedule.json')
-     .map((res: Response) => res.json()); 
+    let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    let body = 'DFH_CURSOR=MONTH&MONTH=1&YEAR=2011&PRICE=100000&DPAY=0&INTREST=5.5&YEARS=30';
+    let welcomebody = 'DFH_CURSOR=&DFH_ENTER=Enter';
+    let url = 'http://192.168.99.100:8080/mort/servlet';
+
+    let resp: Response;
+
+    this.http.get(url).map(this.showData);
+    this.http.post(url, welcomebody,headers).map(this.showData);
+
+    return this.http.post(url, body ,headers)
+     .map(this.showData); 
+
+    // return this.http.get('/app/amortSchedule.json')
+    //  .map((res: Response) => res.json()); 
+  }
+  showData(res: Response) {
+    let data = res.text();
   }
 }
