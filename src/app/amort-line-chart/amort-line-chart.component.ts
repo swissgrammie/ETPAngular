8b85ecd4d296d6934a1332869b238d6f93d49b7e
Graham <graham.cunningham@heirloomcomputing.com>
2016-07-12 11:01:44 +0200

    transfer back to tower

--- src/app/amort-line-chart/amort-line-chart.component.ts
+++ src/app/amort-line-chart/amort-line-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input , ElementRef} from '@angular/core';
 import { AmortItem } from '../amort-item';
-import * as d3 from 'd3';
+import * as D3 from 'd3';
 import * as Moment from 'moment';
 
 @Component({
@@ -29,12 +29,47 @@ export class AmortLineChartComponent {
  **/
  constructor(private element: ElementRef) {
     this.htmlElement = this.element.nativeElement;
-    this.host = d3.select(this.element.nativeElement);
+    this.host = D3.select(this.element.nativeElement);
  }
 
  /**
  * Everythime the @Input is updated, we rebuild the chart
  **/
   ngOnChanges(): void {
+    this.setup();
+    this.buildSVG();
+    this.populate();
+    this.drawXAxis();
+    this.drawYAxis();
   }
+
+  setup() {
+    this.margin = { top: 20, right: 20, bottom: 40, left: 40 };
+    this.width = this.htmlElement.clientWidth - this.margin.left - this.margin.right;
+    this.height = this.width * 0.5 - this.margin.top - this.margin.bottom;
+    this.xScale = D3.time.scale().range([0, this.width]);
+    this.yScale = D3.scale.linear().range([this.height, 0]);    
+  }
+
+  buildSVG() {
+    this.host.html('');
+    this.svg = this.host.append('svg')
+      .attr('width', this.width + this.margin.left + this.margin.right)
+      .attr('height', this.height + this.margin.top + this.margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  }
+
+  populate() {
+
+  }
+
+  drawXAxis() {
+
+  }
+
+  drawYAxis() {
+
+  }
+
 }
